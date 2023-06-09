c63cabd12e076587546004e4cef033d0995a0859
MacBookPro <sulaymon1w@gmail.com>
2023-06-10 01:46:53 +0500

    juz page dark screen bug fixed

--- QuranApp/Views/Juz/JuzRowView.swift
+++ QuranApp/Views/Juz/JuzRowView.swift
@@ -9,45 +9,53 @@ import SwiftUI
 
 struct JuzRowView: View {
     let item: JuzModel
-    
+    @EnvironmentObject var language: LanguageViewModel
+
     var body: some View {
-        NavigationLink(value: Route.menu(item: item.index)) {
-            HStack{
+        var text = LocalizedStringKey("juz").stringValue(locale: language.language)
+        Menu {
+            NavigationLink(value: Route.menu(item: item.index)) {
+                Label("\(item.index) \(text)", systemImage: "timelapse")
+            }
+            ForEach(item.surahs, id: \.index) { surahs in
+                NavigationLink(value: Route.menu(item: surahs.pageNumber.intValue)) {
+                    Label(LocalizedStringKey(surahs.title.localizedForm), systemImage: surahs.type == "Makkiyah" ? "moon.fill" : "sun.max.fill")
+                }
+            }
+        } label: {
+            HStack {
                 Image(systemName: "app")
                     .font(.system(size: 34.0))
                     .overlay {
                         Text("\(item.index)")
                             .font(.system(size: 21.0))
                     }
-                Text("juz")
+                Text(text)
                     .font(.title2)
                 Spacer()
             }
-        }
-        .contextMenu {
-            ForEach(item.surahs, id: \.index) { surahs in
-                NavigationLink(value: Route.menu(item: surahs.pageNumber.intValue)) {
-                    Label(LocalizedStringKey(surahs.title.localizedForm), systemImage: surahs.type == "Makkiyah" ? "moon.fill" : "sun.max.fill")
-                }
-            }
+            .foregroundColor(Color.primary)
         }
     }
 }
 
 struct JuzRowView_Previews: PreviewProvider {
     static var previews: some View {
-        List{
-            JuzRowView(item: JuzModel(index: 1, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "2",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 1, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "2",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 2, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "1",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 5, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "9",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 17, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "1",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 0, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "4",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 04, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "6",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
-            JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
+        NavigationStack {
+            List {
+                JuzRowView(item: JuzModel(index: 1, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "2",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 1, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "2",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 2, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "1",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 5, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "9",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 17, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "1",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 0, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "4",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 04, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "6",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
+                JuzRowView(item: JuzModel(index: 12, page:1, surahs: [Surah(titleAr: "", title: "Al-Fatiha", index: 2, verse: VerseNew(start: 12, end: 12), pageNumber: "12",type: "Makkiyah")]))
+            }
+            .environment(\.locale, Locale.init(identifier: "ru"))
+            .environmentObject(LanguageViewModel())
         }
-        .environment(\.locale, Locale.init(identifier: "ru"))
     }
 }
