d61b87860a4a3539f3cdb11fba8ff08250ce5044
MacBookPro <sulaymon1w@gmail.com>
2023-06-15 15:01:28 +0500

    last page added new items

--- QuranApp/Views/Bookmark/LastPage/LastPageView.swift
+++ QuranApp/Views/Bookmark/LastPage/LastPageView.swift
@@ -10,28 +10,42 @@ import SwiftUI
 struct LastPageView: View {
     @EnvironmentObject var routerManager: RouterManager
     @EnvironmentObject var surahViewModel: SurahViewModel
-    
+    @EnvironmentObject var bookmarksViewModel: BookMarkViewModel
+
     var body: some View {
         let item = surahViewModel.getSurahByPage(routerManager.currentPDFPage)!
+        let status = bookmarksViewModel.getPages().contains((item.pages as NSString).integerValue)
         Section("lastReadPage") {
-            BookmarkRowView(title: item.title, juz: item.juz[0].index, pageNumber: routerManager.currentPDFPage)
-                .overlay {
-                    NavigationLink(value: Route.menu(item: routerManager.currentPDFPage.toString)) {
-                        Text(">>>")
+            ForEach(0...0, id: \.self) { number in
+                BookmarkRowView(title: item.title, juz: item.juz[0].index, pageNumber: routerManager.currentPDFPage)
+                    .overlay {
+                        NavigationLink(value: Route.menu(item: routerManager.currentPDFPage.toString)) {
+                            Text(">>>")
+                        }
+                        .opacity(0)
                     }
-                    .opacity(0)
-                }
+                    .swipeActions(edge: .trailing, allowsFullSwipe: !status) {
+                        BookmarkSwipe(item: BookmarkModel(title: item.title, juz: item.juz[0].index, pageNumber: item.pages.intValue), status: status)
+                            .tint(status ? .red : .green)
+                    }
+                    .swipeActions(edge: .leading) {
+                        ShareSwipe(title: LocalizedStringKey(item.title).stringValue(), index: item.pages)
+                    }
+            }
         }
     }
 }
 
 struct LastPageView_Previews: PreviewProvider {
     static var previews: some View {
-        List {
-            LastPageView()
+        NavigationStack {
+            List {
+                LastPageView()
+            }
         }
         .environmentObject(LanguageViewModel())
         .environmentObject(RouterManager())
         .environmentObject(SurahViewModel())
+        .environmentObject(BookMarkViewModel())
     }
 }
